408c7b9c4a4bf0b06d19dffa503306ec1cf7b1a9
sriramh14 <sriramhari1014@gmail.com>
2026-06-22 18:31:59 +0530

    Update train4.py

--- DiffIR_RGB2HSI/DiffIR_RGB2HSI/train4.py
+++ DiffIR_RGB2HSI/DiffIR_RGB2HSI/train4.py
@@ -174,7 +174,7 @@ USE_RGB_TO_HSI_SKIP = False
 
 # Stage-2 diffusion.
 N_DENOISE_RES = 10               #original value was 2
-DIFFUSION_TIMESTEPS = 25        #original value was 4
+DIFFUSION_TIMESTEPS = 192        #original value was 4
 
 #Original schedule was 0.1 to 0.99
 LINEAR_START = 0.1            
