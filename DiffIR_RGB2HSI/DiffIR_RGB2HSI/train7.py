ee3e10024851506cdd829955991bd3fe35860b80
sriramh14 <sriramhari1014@gmail.com>
2026-06-29 13:47:53 +0530

    Update train7.py

--- DiffIR_RGB2HSI/DiffIR_RGB2HSI/train7.py
+++ DiffIR_RGB2HSI/DiffIR_RGB2HSI/train7.py
@@ -42,8 +42,7 @@ from torch.utils.checkpoint import checkpoint as activation_checkpoint
 
 from loss import compute_metrics, prior_kd_loss, prior_l1_loss, reconstruction_loss
 
-#Change this to metamer_aware_model or spec_prior_model or BBDM_ver_diffIR or new_prior_without_pix_ushufl or stg2_combined_dm
-from models.spec_prior_model import DiffIRS1RGB2HSI, DiffIRS2RGB2HSI, ModelConfig
+from models.spec_prior_with_hsi_weight import DiffIRS1RGB2HSI, DiffIRS2RGB2HSI, ModelConfig
 
 STAGE1_ARCHITECTURE_KEYS = {
     "num_bands",
